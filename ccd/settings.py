1e666a1579c2043f53dabd5b12d4dc5fef0c3ef4
Rushikesh0824 <rushikesh.wadekar@theagilemorph.com>
2025-10-27 15:33:55 +0530

    Latest : Latest changes by 26 oct.

--- ccd/settings.py
+++ ccd/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER', 'nexusccd_user'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5433'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
